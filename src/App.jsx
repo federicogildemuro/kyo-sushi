dcfca5977f9f1ec462abe929c7cfe2eca9df91f7
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-11-05 19:08:27 -0300

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import 'normalize.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
+import Cart from './components/Cart/Cart'
+import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 
 function App() {
     return (
-        <>
-            <header>
-                <NavBar />
-            </header>
+        <BrowserRouter>
+            <NavBar />
 
-            <main>
-                <ItemListContainer />
-            </main>
+            <Routes>
+                <Route path="/" element={<ItemListContainer />} />
+                <Route path="/category/:id" element={<ItemListContainer />} />
+                <Route path="/item/:id" element={<ItemDetailContainer />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<h1>404 Not Found</h1>} />
+            </Routes>
 
-            <footer>
-                <Footer />
-            </footer>
-        </>
+            <Footer />
+        </BrowserRouter>
     )
 }
 
