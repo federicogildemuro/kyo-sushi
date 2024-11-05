f0215c04b006a562959b1e09df1fb4cae317a7c0
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-11-05 19:43:57 -0300

    update item list container and item list components

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,12 +1,15 @@
-import Item from "../Item/Item"
+import './ItemList.css'
+import Item from '../Item/Item'
 
 function ItemList({ items }) {
     return (
-        <ul>
-            {items.map((item) => (
-                <Item key={item.id} item={item} />
-            ))}
-        </ul>
+        <div className="container-fluid">
+            <ul className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+                {items.map((item) => (
+                    <Item key={item.id} item={item} />
+                ))}
+            </ul>
+        </div>
     )
 }
 
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,28 +1,52 @@
+import './ItemListContainer.css'
 import { useEffect, useState } from 'react'
-import { getProducts } from '../../services/ProductsServices'
+import { useParams } from 'react-router-dom'
+import { getProducts, getProductsByCategory } from '../../services/ProductsServices'
 import ItemList from '../ItemList/ItemList'
 
-function ItemListContainer({ greeting }) {
-    const [products, setProducts] = useState([]);
+function ItemListContainer() {
+    const [h1, setH1] = useState('Bienvenidos a Kyo Sushi');
+    const [items, setItems] = useState([]);
+    const { id: category } = useParams();
+
+    const fetchItems = async () => {
+        try {
+            const data = await getProducts();
+            setItems(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const fetchItemsByCategory = async () => {
+        try {
+            const data = await getProductsByCategory(category);
+            setItems(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const data = await getProducts();
-                setProducts(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        fetchProducts();
-    }, []);
+        if (category) {
+            fetchItemsByCategory();
+            setH1(`${category}`);
+        } else {
+            fetchItems();
+            setH1('Bienvenidos a Kyo Sushi');
+        }
+    }, [category]);
 
     return (
         <>
-            <h1>{greeting}</h1>
-            <ItemList items={products} />
+            <h1>{h1}</h1>
+            {items.length > 0 ? (
+                <ItemList items={items} />
+            ) : (
+                <p>No hay productos disponibles</p>
+            )}
         </>
-    )
+    );
 }
 
 export default ItemListContainer
