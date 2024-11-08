ae92eb3b29f384d7993d4d779913bd116d3043fa
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-11-08 16:29:17 -0300

    add spinner component

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,11 @@
+import './Spinner.css'
+
+function Spinner() {
+    return (
+        <div className="d-flex justify-content-center align-items-center">
+            <div className="spinner-border spinner-custom" />
+        </div>
+    )
+}
+
+export default Spinner
