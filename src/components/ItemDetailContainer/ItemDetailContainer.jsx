36e3356c678af6c286456efbd0bcd4aa02ecfe64
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-11-08 16:30:55 -0300

    add spinner to item list container and item detail container

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,27 +1,37 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { getProduct } from '../../services/ProductsServices'
+import Spinner from '../Spinner/Spinner'
 import ItemDetail from '../ItemDetail/ItemDetail'
 
 function ItemDetailContainer() {
     const { id } = useParams();
     const [item, setItem] = useState({});
+    const [loading, setLoading] = useState(true);
 
     const fetchItem = async (id) => {
         try {
             const data = await getProduct(id);
             setItem(data);
+            setLoading(false);
         } catch (error) {
             console.error(error);
         }
     };
 
     useEffect(() => {
+        setLoading(true);
         fetchItem(id);
     }, [id]);
 
     return (
-        <ItemDetail item={item} />
+        <>
+            {loading && <Spinner />}
+
+            {!loading && item && <ItemDetail item={item} />}
+
+            {!loading && !item && <p>No se encontró el producto</p>}
+        </>
     )
 }
 
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,17 +2,20 @@ import './ItemListContainer.css'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getProducts, getProductsByCategory } from '../../services/ProductsServices'
+import Spinner from '../Spinner/Spinner'
 import ItemList from '../ItemList/ItemList'
 
 function ItemListContainer() {
     const [h1, setH1] = useState('Bienvenidos a Kyo Sushi');
     const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
     const { id: category } = useParams();
 
     const fetchItems = async () => {
         try {
             const data = await getProducts();
             setItems(data);
+            setLoading(false);
         } catch (error) {
             console.error(error);
         }
@@ -22,6 +25,7 @@ function ItemListContainer() {
         try {
             const data = await getProductsByCategory(category);
             setItems(data);
+            setLoading(false);
         } catch (error) {
             console.error(error);
         }
@@ -29,9 +33,11 @@ function ItemListContainer() {
 
     useEffect(() => {
         if (category) {
+            setLoading(true);
             fetchItemsByCategory();
             setH1(`${category}`);
         } else {
+            setLoading(true);
             fetchItems();
             setH1('Bienvenidos a Kyo Sushi');
         }
@@ -40,11 +46,12 @@ function ItemListContainer() {
     return (
         <>
             <h1>{h1}</h1>
-            {items.length > 0 ? (
-                <ItemList items={items} />
-            ) : (
-                <p>No hay productos disponibles</p>
-            )}
+
+            {loading && <Spinner />}
+
+            {!loading && items.length > 0 && <ItemList items={items} />}
+
+            {!loading && items.length === 0 && <p>No hay productos disponibles</p>}
         </>
     );
 }
