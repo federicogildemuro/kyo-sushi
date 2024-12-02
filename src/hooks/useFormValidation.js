47714f73ca51231cb06321b44e79e50794a2a5f5
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-12-02 17:43:19 -0300

    add use form validation hook

---
+++ src/hooks/useFormValidation.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { validateField, validateForm, isFormValid } from "../utils/ValidationUtils";
+
+export const useFormValidation = (initialData) => {
+    const [formData, setFormData] = useState(initialData);
+    const [formErrors, setFormErrors] = useState({});
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+
+        setFormErrors((prev) => ({
+            ...prev,
+            [name]: validateField(name, value, formData),
+        }));
+    };
+
+    const handleBlur = (e) => {
+        const { name, value } = e.target;
+        setFormErrors((prev) => ({
+            ...prev,
+            [name]: validateField(name, value, formData),
+        }));
+    };
+
+    const validateFormData = () => {
+        const errors = validateForm(formData);
+        setFormErrors(errors);
+        return isFormValid(errors);
+    };
+
+    return { formData, formErrors, handleInputChange, handleBlur, validateFormData };
+};
