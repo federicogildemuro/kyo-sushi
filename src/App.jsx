9845731841a0a50aa815c160a85241fad457d84d
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-12-03 16:23:48 -0300

    add about us component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import Cart from './components/Cart/Cart'
 import Checkout from './components/Checkout/Checkout'
+import AboutUs from './components/AboutUs/AboutUs'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import 'normalize.css'
@@ -24,6 +25,7 @@ function App() {
                     <Route path="/item/:id" element={<ItemDetailContainer />} />
                     <Route path="/cart" element={<Cart />} />
                     <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/about-us" element={<AboutUs />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="*" element={<LandingPage />} />
                 </Routes>
