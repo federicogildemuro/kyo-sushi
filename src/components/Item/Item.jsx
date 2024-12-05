316209949e72ebc78407f0f1203087f9dea6cde4
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-12-05 20:45:23 -0300

    refactor item component

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,16 +1,26 @@
+import { Link } from 'react-router-dom'
+import { scrollToTop } from '../../utils/ScrollUtils'
 import './Item.css'
-import { NavLink } from 'react-router-dom'
 
 function Item({ item }) {
+
+    if (!item) return null;
+
     return (
         <li className="col">
-            <div className="card h-100">
+            <div className="item-container card h-100">
                 <img src={item.pictureUrl} className="card-img-top" alt={item.title} />
-                <div className="card-body">
+                <div className="card-body d-flex flex-column justify-content-between align-items-center">
                     <p className="card-text">{item.category}</p>
                     <h5 className="card-title">{item.title}</h5>
                     <p className="card-text">€{item.price.toFixed(2)}</p>
-                    <NavLink to={`/item/${item.id}`} className="btn btn-primary">Ver detalle</NavLink>
+                    <Link
+                        to={`/item/${item.id}`}
+                        onClick={scrollToTop}
+                        className="btn custom-btn w-auto"
+                    >
+                        Ver detalle
+                    </Link>
                 </div>
             </div>
         </li>
