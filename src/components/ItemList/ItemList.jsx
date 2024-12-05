eb4d6c0762730463d2a8689442a0f102cc3fa562
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-12-05 20:45:12 -0300

    refactor item list component

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,10 +1,13 @@
-import './ItemList.css'
 import Item from '../Item/Item'
+import './ItemList.css'
 
 function ItemList({ items }) {
+
+    if (!items) return null;
+
     return (
         <div className="container-fluid">
-            <ul className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+            <ul className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 m-3 mb-5">
                 {items.map((item) => (
                     <Item key={item.id} item={item} />
                 ))}
