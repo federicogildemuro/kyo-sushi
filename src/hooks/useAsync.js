d35bd1a52bac629a4eb5715a2f3ae39ae75d23fd
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-12-05 12:42:07 -0300

    refactor use async hook

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -1,19 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 function useAsync(asyncFunction, dependencies = []) {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
+    const execute = async (...args) => {
         setLoading(true);
-        asyncFunction()
-            .then((data) => setData(data))
-            .catch((error) => setError(error))
-            .finally(() => setLoading(false));
+        try {
+            const result = await asyncFunction(...args);
+            setData(result);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        execute();
     }, dependencies);
 
-    return { data, loading, error };
+    return { data, loading, error, execute }
 }
 
-export default useAsync;
+export default useAsync
