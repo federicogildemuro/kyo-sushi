093a44ff652fc34a72f94e547513fe8638007a4b
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-12-05 19:20:40 -0300

    refactor notification context

--- src/contexts/NotificationContext.jsx
+++ src/contexts/NotificationContext.jsx
@@ -1,21 +1,40 @@
-import { createContext, useState, useCallback } from 'react'
+import { createContext, useState, useCallback, useEffect } from 'react'
+import './Notification.css'
 
 const NotificationContext = createContext();
 
 function NotificationProvider({ children }) {
     const [notification, setNotification] = useState(null);
+    const [isFadingOut, setIsFadingOut] = useState(false);
 
     const showNotification = useCallback((message, type) => {
         setNotification({ message, type });
-        setTimeout(() => setNotification(null), 5000);
+        setIsFadingOut(false);
     }, []);
+
+    useEffect(() => {
+        if (notification) {
+            const fadeOutTimer = setTimeout(() => {
+                setIsFadingOut(true);
+            }, 4500);
+
+            const clearNotificationTimer = setTimeout(() => {
+                setNotification(null);
+            }, 5000);
+
+            return () => {
+                clearTimeout(fadeOutTimer);
+                clearTimeout(clearNotificationTimer);
+            }
+        }
+    }, [notification]);
 
     return (
         <NotificationContext.Provider value={{ showNotification }}>
             {children}
             {notification && (
                 <div
-                    className={`toast show position-fixed top-0 start-50 mt-5 translate-middle-x bg-${notification.type}`}
+                    className={`toast show position-fixed top-0 start-50 mt-5 translate-middle-x bg-${notification.type} ${isFadingOut ? 'fade-out' : 'fade-in'}`}
                     role="alert"
                     style={{
                         zIndex: 1000,
