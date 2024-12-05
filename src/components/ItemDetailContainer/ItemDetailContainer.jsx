e7410ca6792d5ba4b91b9485a6f6dba12f3601f3
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-12-05 18:41:04 -0300

    refactor item detail container component

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,20 +1,28 @@
-import { useParams } from 'react-router-dom';
-import { fetchProductById } from '../../services/ProductsServices';
-import useAsync from '../../hooks/useAsync';
-import Spinner from '../Spinner/Spinner';
-import ItemDetail from '../ItemDetail/ItemDetail';
+import { useParams, Link } from 'react-router-dom'
+import { fetchProductById } from '../../services/ProductsServices'
+import useAsync from '../../hooks/useAsync'
+import Spinner from '../Spinner/Spinner'
+import ItemDetail from '../ItemDetail/ItemDetail'
+import './ItemDetailContainer.css'
 
 function ItemDetailContainer() {
     const { id } = useParams();
-    const { data, loading, error } = useAsync(() => fetchProductById(id), [id]);
+    const { data, loading } = useAsync(() => fetchProductById(id), [id]);
 
-    if (loading) return <Spinner />;
-
-    if (error) return <p>Error al cargar el producto</p>;
-
-    if (!data) return <p>Producto no encontrado</p>;
-
-    return <ItemDetail item={data} />;
+    return (
+        <section className="item-detail-container d-flex flex-column text-center">
+            {loading ? (
+                <Spinner />
+            ) : data ? (
+                <ItemDetail item={data} />
+            ) : (
+                <>
+                    <p className="fs-5 fs-sm-6 fs-md-7 fs-lg-8 mt-3 mb-3">Producto no encontrado</p>
+                    <Link to="/tienda" className="btn custom-btn mx-auto">Volver a la tienda</Link>
+                </>
+            )}
+        </section>
+    )
 }
 
-export default ItemDetailContainer;
+export default ItemDetailContainer
