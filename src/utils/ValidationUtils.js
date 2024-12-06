16559c62ec1c8b09d957095e4753bdec65fc9651
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-12-06 08:41:46 -0300

    refactor validation utils

--- src/utils/ValidationUtils.js
+++ src/utils/ValidationUtils.js
@@ -1,25 +1,20 @@
 const validateField = (name, value, formData) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    const errors = {
-        name: !value.trim() ? "El nombre es obligatorio" : "",
-        email: !value.trim()
-            ? "El email es obligatorio"
-            : !emailRegex.test(value)
-                ? "El email no es válido"
-                : "",
-        confirmEmail: !value.trim()
-            ? "Debes confirmar el email"
-            : value !== formData.email
-                ? "Los correos no coinciden"
-                : "",
-        phone: !value.trim() ? "El teléfono es obligatorio" : "",
-        street: !value.trim() ? "La calle es obligatoria" : "",
-        number: !value.trim() ? "La altura es obligatoria" : "",
-    };
+    if (!value.trim() && name !== "apartment") {
+        return "Campo obligatorio";
+    }
 
-    return errors[name] || "";
-}
+    if (name === "email" && !emailRegex.test(value)) {
+        return "El correo no es válido";
+    }
+
+    if (name === "confirmEmail" && value !== formData.email) {
+        return "Los correos no coinciden";
+    }
+
+    return "";
+};
 
 const validateForm = (formData) => {
     const errors = {};
@@ -29,7 +24,7 @@ const validateForm = (formData) => {
     });
 
     return errors;
-}
+};
 
 const isFormValid = (errors) => Object.values(errors).every((error) => !error);
 
