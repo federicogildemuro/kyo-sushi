62c5c4fb2c2570c7b9d853004aa4d5507badb593
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-12-06 09:25:57 -0300

    refactor about us component

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import './AboutUs.css'
 
 function AboutUs() {
     useEffect(() => {
@@ -17,6 +16,7 @@ function AboutUs() {
                 <h1 className="display-6 fw-bold mb-3" data-aos="fade-down">
                     Sobre nosotros
                 </h1>
+
                 <div>
                     <p className="fs-5 fs-sm-6 fs-md-7 fs-lg-8 lh-lg mb-3" data-aos="fade-up" data-aos-delay="200">
                         Somos <strong>Tefo</strong> y <strong>Braichu</strong>, dos amigos emprendedores de
@@ -24,12 +24,14 @@ function AboutUs() {
                         años de experiencia trabajando en el mundo de la cocina, decidimos unir nuestras fuerzas y dar vida
                         a un proyecto que refleja nuestra dedicación y amor por la buena comida: <strong>Kyo Sushi</strong>.
                     </p>
+
                     <p className="fs-5 fs-sm-6 fs-md-7 fs-lg-8 lh-lg mb-3" data-aos="fade-up" data-aos-delay="400">
                         Nuestro objetivo es llevar a tu mesa lo mejor de la cocina japonesa, con ingredientes frescos
                         y de la más alta calidad. En cada roll, sashimi y nigiri, ponemos toda nuestra creatividad y
                         conocimientos para ofrecerte una experiencia gastronómica única que combina tradición y un
                         toque de innovación.
                     </p>
+
                     <p className="fs-5 fs-sm-6 fs-md-7 fs-lg-8 lh-lg mb-3" data-aos="fade-up" data-aos-delay="600">
                         En <strong>Kyo Sushi</strong>, creemos que la comida no solo alimenta, sino que también conecta
                         a las personas. Por eso, trabajamos día a día para que cada pedido sea una celebración de sabores,
