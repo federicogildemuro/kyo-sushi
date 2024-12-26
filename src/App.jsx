1b0a67cfff45ce3a97fb1036437e45babe4e51af
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-12-26 06:16:01 -0300

    add lazy loading to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { NotificationProvider } from './contexts/NotificationContext'
-import { CartProvider } from './contexts/CartContext'
-import NavBar from './components/NavBar/NavBar'
-import LandingPage from './components/LandingPage/LandingPage'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
-import Cart from './components/Cart/Cart'
-import Checkout from './components/Checkout/Checkout'
-import AboutUs from './components/AboutUs/AboutUs'
-import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
-import 'normalize.css'
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { NotificationProvider } from './contexts/NotificationContext';
+import { CartProvider } from './contexts/CartContext';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import Spinner from './components/Spinner/Spinner';
+import 'normalize.css';
+
+const LandingPage = lazy(() => import('./components/LandingPage/LandingPage'));
+const ItemListContainer = lazy(() => import('./components/ItemListContainer/ItemListContainer'));
+const ItemDetailContainer = lazy(() => import('./components/ItemDetailContainer/ItemDetailContainer'));
+const Cart = lazy(() => import('./components/Cart/Cart'));
+const Checkout = lazy(() => import('./components/Checkout/Checkout'));
+const AboutUs = lazy(() => import('./components/AboutUs/AboutUs'));
+const Contact = lazy(() => import('./components/Contact/Contact'));
 
 function App() {
     return (
@@ -19,23 +22,25 @@ function App() {
                 <CartProvider>
                     <NavBar />
 
-                    <Routes>
-                        <Route path="/" element={<LandingPage />} />
-                        <Route path="/tienda/" element={<ItemListContainer />} />
-                        <Route path="/tienda/:category" element={<ItemListContainer />} />
-                        <Route path="/item/:id" element={<ItemDetailContainer />} />
-                        <Route path="/cart" element={<Cart />} />
-                        <Route path="/checkout" element={<Checkout />} />
-                        <Route path="/about-us" element={<AboutUs />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="*" element={<LandingPage />} />
-                    </Routes>
+                    <Suspense fallback={<Spinner />}>
+                        <Routes>
+                            <Route path="/" element={<LandingPage />} />
+                            <Route path="/tienda/" element={<ItemListContainer />} />
+                            <Route path="/tienda/:category" element={<ItemListContainer />} />
+                            <Route path="/item/:id" element={<ItemDetailContainer />} />
+                            <Route path="/cart" element={<Cart />} />
+                            <Route path="/checkout" element={<Checkout />} />
+                            <Route path="/about-us" element={<AboutUs />} />
+                            <Route path="/contact" element={<Contact />} />
+                            <Route path="*" element={<LandingPage />} />
+                        </Routes>
+                    </Suspense>
 
                     <Footer />
                 </CartProvider>
             </NotificationProvider>
         </BrowserRouter>
-    )
+    );
 }
 
-export default App
+export default App;
