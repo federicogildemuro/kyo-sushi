e969c0bbf920a3a776e967fb2cc3d60693525972
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-01-09 11:25:05 -0300

    add wishlist context and component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { NotificationProvider } from './contexts/NotificationContext';
 import { CartProvider } from './contexts/CartContext';
+import { WishlistProvider } from './contexts/WishlistContext';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import Spinner from './components/Spinner/Spinner';
@@ -11,6 +12,7 @@ const LandingPage = lazy(() => import('./components/LandingPage/LandingPage'));
 const ItemListContainer = lazy(() => import('./components/ItemListContainer/ItemListContainer'));
 const ItemDetailContainer = lazy(() => import('./components/ItemDetailContainer/ItemDetailContainer'));
 const Cart = lazy(() => import('./components/Cart/Cart'));
+const Wishlist = lazy(() => import('./components/Wishlist/Wishlist'));
 const Checkout = lazy(() => import('./components/Checkout/Checkout'));
 const AboutUs = lazy(() => import('./components/AboutUs/AboutUs'));
 const Contact = lazy(() => import('./components/Contact/Contact'));
@@ -20,23 +22,26 @@ function App() {
         <BrowserRouter>
             <NotificationProvider>
                 <CartProvider>
-                    <NavBar />
+                    <WishlistProvider>
+                        <NavBar />
 
-                    <Suspense fallback={<Spinner />}>
-                        <Routes>
-                            <Route path="/" element={<LandingPage />} />
-                            <Route path="/tienda/" element={<ItemListContainer />} />
-                            <Route path="/tienda/:category" element={<ItemListContainer />} />
-                            <Route path="/item/:id" element={<ItemDetailContainer />} />
-                            <Route path="/cart" element={<Cart />} />
-                            <Route path="/checkout" element={<Checkout />} />
-                            <Route path="/about-us" element={<AboutUs />} />
-                            <Route path="/contact" element={<Contact />} />
-                            <Route path="*" element={<LandingPage />} />
-                        </Routes>
-                    </Suspense>
+                        <Suspense fallback={<Spinner />}>
+                            <Routes>
+                                <Route path="/" element={<LandingPage />} />
+                                <Route path="/tienda/" element={<ItemListContainer />} />
+                                <Route path="/tienda/:category" element={<ItemListContainer />} />
+                                <Route path="/item/:id" element={<ItemDetailContainer />} />
+                                <Route path="/cart" element={<Cart />} />
+                                <Route path="/wishlist" element={<Wishlist />} />
+                                <Route path="/checkout" element={<Checkout />} />
+                                <Route path="/about-us" element={<AboutUs />} />
+                                <Route path="/contact" element={<Contact />} />
+                                <Route path="*" element={<LandingPage />} />
+                            </Routes>
+                        </Suspense>
 
-                    <Footer />
+                        <Footer />
+                    </WishlistProvider>
                 </CartProvider>
             </NotificationProvider>
         </BrowserRouter>
