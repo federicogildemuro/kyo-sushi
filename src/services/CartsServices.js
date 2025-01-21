fdc749f8cf1daae8d447e9084b69751bc8b67a5f
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-01-21 11:44:33 -0300

    refactor cart services

--- src/services/CartsServices.js
+++ src/services/CartsServices.js
@@ -1,21 +1,21 @@
 const getCart = () => {
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
     return [...cart];
-}
+};
 
 const saveCart = (cart) => localStorage.setItem('cart', JSON.stringify(cart));
 
 const isItemInCart = (id) => {
     const cart = getCart();
     return cart.some((item) => item.id === id);
-}
+};
 
 const addItemToCart = (item) => {
     let cart = getCart();
     if (isItemInCart(item.id)) {
         cart = cart.map((existingItem) =>
             existingItem.id === item.id
-                ? { ...existingItem, quantity: existingItem.quantity + item.quantity }
+                ? { ...existingItem, quantity: item.quantity }
                 : existingItem
         );
     } else {
@@ -23,23 +23,23 @@ const addItemToCart = (item) => {
     }
     saveCart(cart);
     return getCart();
-}
+};
 
 const removeItemFromCart = (id) => {
     let cart = getCart();
     cart = cart.filter((item) => item.id !== id);
     saveCart(cart);
     return getCart();
-}
+};
 
 const calculateCartTotal = () => {
     const cart = getCart();
     return cart.reduce((total, item) => total + item.price * item.quantity, 0);
-}
+};
 
 const clearCartItems = () => {
     saveCart([]);
     return getCart();
-}
+};
 
-export { getCart, isItemInCart, addItemToCart, removeItemFromCart, calculateCartTotal, clearCartItems }
+export { getCart, isItemInCart, addItemToCart, removeItemFromCart, calculateCartTotal, clearCartItems };
