baca29a25d43da927fb0b9d6097d7ee245cd7de5
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-01-22 17:48:10 -0300

    refactor cart widget component

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -9,14 +9,18 @@ function CartWidget() {
     return (
         <Link
             to="/cart"
-            className="nav-link"
+            className="nav-link position-relative"
             onClick={scrollToTop}
         >
-            <i className="cart-widget-icon bi bi-cart" />
+            <i className="cart-widget-icon fs-2 bi bi-cart" />
 
-            {cartQuantity > 0 && cartQuantity < 10 && <span className="cart-widget-quantity">{cartQuantity}</span>}
-
-            {cartQuantity > 10 && <span className="cart-widget-quantity">+10</span>}
+            {cartQuantity > 0 && (
+                <span
+                    className="cart-widget-quantity position-absolute top-0 end-0 translate-middle text-white bg-danger d-flex align-items-center justify-content-center rounded-circle fw-bold"
+                >
+                    {cartQuantity < 10 ? cartQuantity : '+10'}
+                </span>
+            )}
         </Link>
     );
 }
