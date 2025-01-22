651137c7355ffc40fdeed444229b908077428025
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-01-22 19:12:49 -0300

    refactor cart item component

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,32 +1,24 @@
 import { Link } from 'react-router-dom';
-import useCart from '../../hooks/useCart';
-import useNotification from '../../hooks/useNotification';
 import './CartItem.css';
 
-function CartItem({ item }) {
-    const { removeCartItem } = useCart();
-    const { showNotification } = useNotification();
-
-    const handleRemoveCartItem = () => {
-        removeCartItem(item.id);
-        showNotification(`${item.title} eliminado del carrito exitosamente`, 'success');
-    }
-
+function CartItem({ item, onRemoveFromCart }) {
     if (!item) return null;
 
+    const { id, title, quantity, price } = item;
+
     return (
-        <tr key={item.id}>
-            <td className="text-start">{item.title}</td>
+        <tr>
+            <td className="text-start">{title}</td>
 
-            <td className="text-center d-none d-sm-table-cell d-md-table-cell">{item.quantity}</td>
+            <td className="text-center d-none d-sm-table-cell d-md-table-cell">{quantity}</td>
 
-            <td className="text-end d-none d-sm-table-cell d-md-table-cell">${item.price.toFixed(2)}</td>
+            <td className="text-end d-none d-sm-table-cell d-md-table-cell">${price.toFixed(2)}</td>
 
-            <td className="text-end">${(item.price * item.quantity).toFixed(2)}</td>
+            <td className="text-end">${(price * quantity).toFixed(2)}</td>
 
             <td className="d-flex justify-content-end gap-2">
                 <Link
-                    to={`/item/${item.id}`}
+                    to={`/item/${id}`}
                     className="btn custom-btn btn-sm"
                 >
                     <i className="bi bi-eye" />
@@ -34,14 +26,13 @@ function CartItem({ item }) {
 
                 <button
                     className="btn custom-btn btn-sm"
-                    onClick={handleRemoveCartItem}
+                    onClick={() => onRemoveFromCart(id)}
                 >
                     <i className="bi bi-trash" />
-
                 </button>
             </td>
         </tr>
-    )
+    );
 }
 
 export default CartItem;
