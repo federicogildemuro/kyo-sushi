53b74a4a22ab6fdd7f46e84f5a72047313fdfcf0
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-01-22 08:38:55 -0300

    add message prop

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,13 +1,15 @@
-import './Spinner.css'
+import './Spinner.css';
 
-function Spinner() {
+function Spinner({ message }) {
     return (
         <div className="spinner-overlay">
-            <div className="d-flex justify-content-center align-items-center">
+            <div className="d-flex flex-column justify-content-center align-items-center gap-3">
                 <div className="spinner-border spinner-custom" />
+
+                {message && <p className="spinner-message">{message}</p>}
             </div>
         </div>
-    )
+    );
 }
 
-export default Spinner
+export default Spinner;
