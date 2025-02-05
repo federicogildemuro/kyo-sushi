a89d5cc407de6ffc9f72585f75823d89a5c2c119
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-05 12:15:55 -0300

    add create user adapter

---
+++ src/adapters/createUserAdapter.js
@@ -0,0 +1,18 @@
+const createUserAdapter = (formData) => ({
+    firstName: formData.firstName,
+    lastName: formData.lastName,
+    email: formData.email,
+    phone: formData.phone,
+    address: {
+        street: formData.street,
+        number: formData.number,
+        apartment: formData.apartment,
+        city: formData.city,
+        state: formData.state,
+        country: formData.country
+    },
+    role: 'user',
+    lastLogin: new Date().toISOString()
+});
+
+export default createUserAdapter;
