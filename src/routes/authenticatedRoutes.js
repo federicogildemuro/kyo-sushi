3232ce32c13a8d1c60175328fa72302b24e9e6f2
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-07 21:54:57 -0300

    add checkout and order-confirmation routes

--- src/routes/authenticatedRoutes.js
+++ src/routes/authenticatedRoutes.js
@@ -3,11 +3,13 @@ import { lazy } from 'react';
 const Cart = lazy(() => import('../pages/shop/cart/Cart'));
 const Favorites = lazy(() => import('../pages/shop/favorites/Favorites'));
 const Checkout = lazy(() => import('../pages/shop/checkout/Checkout'));
+const OrderConfirmation = lazy(() => import('../pages/shop/OrderConfirmation'));
 
 const userRoutes = [
     { path: '/cart', component: Cart },
     { path: '/favorites', component: Favorites },
-    { path: '/checkout', component: Checkout }
+    { path: '/checkout', component: Checkout },
+    { path: '/order-confirmation/:orderId', component: OrderConfirmation }
 ];
 
 export default userRoutes;
