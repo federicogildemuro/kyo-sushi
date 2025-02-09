c5d97ede320b30fe15ad318a072a55d28b7d9754
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-09 20:49:36 -0300

    add profile route

--- src/routes/authenticatedRoutes.js
+++ src/routes/authenticatedRoutes.js
@@ -1,11 +1,13 @@
 import { lazy } from 'react';
 
+const UnderConstruction = lazy(() => import('../pages/misc/UnderConstruction'));
 const Cart = lazy(() => import('../pages/cart/Cart'));
 const Favorites = lazy(() => import('../pages/favorites/Favorites'));
 const Checkout = lazy(() => import('../pages/checkout/Checkout'));
 const OrderConfirmation = lazy(() => import('../pages/checkout/OrderConfirmation'));
 
 const userRoutes = [
+    { path: '/profile', component: UnderConstruction },
     { path: '/cart', component: Cart },
     { path: '/favorites', component: Favorites },
     { path: '/checkout', component: Checkout },
