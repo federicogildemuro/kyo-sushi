7983d983cbd99abf488688c943de28304c165be8
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-09 20:22:34 -0300

    update admin routes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,16 +3,16 @@ import { lazy } from 'react';
 const AdminDashboard = lazy(() => import('../pages/admin/AdminDashboard'));
 const AdminProducts = lazy(() => import('../pages/admin/AdminProducts'));
 const ProductForm = lazy(() => import('../pages/admin/ProductForm'));
-const AdminOrders = lazy(() => import('../pages/admin/AdminOrders'));
-const OrderForm = lazy(() => import('../pages/admin/OrderForm'));
+const UnderConstruction = lazy(() => import('../pages/misc/UnderConstruction'));
 
 const adminRoutes = [
     { path: '/admin', component: AdminDashboard },
     { path: '/admin/products', component: AdminProducts },
     { path: '/admin/products/new', component: ProductForm },
     { path: '/admin/products/:id', component: ProductForm },
-    { path: '/admin/orders', component: AdminOrders },
-    { path: '/admin/orders/:id', component: OrderForm }
+    { path: '/admin/categories', component: UnderConstruction },
+    { path: '/admin/orders', component: UnderConstruction },
+    { path: '/admin/users', component: UnderConstruction }
 ];
 
 export default adminRoutes;
