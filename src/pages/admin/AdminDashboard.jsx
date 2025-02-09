6807a4397e02c0f0536d7a0810f8c7819469caf6
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-09 20:24:38 -0300

    refacto admin dashboard page

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -13,14 +13,34 @@ function AdminDashboard() {
                             <div className="card-body">
                                 <h2 className="card-title">Productos</h2>
 
-                                <p className="card-text">Administra los productos de la tienda.</p>
+                                <p className="card-text">Administra los productos de la tienda</p>
 
                                 <Link
                                     to="/admin/products"
                                     className="btn custom-btn"
                                     onClick={scrollToTop}
                                 >
-                                    Administrar
+                                    Ir a productos
+                                    <i className="bi bi-box-seam ms-2" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div className="card custom-card mb-4">
+                            <div className="card-body">
+                                <h2 className="card-title">Categorías</h2>
+
+                                <p className="card-text">Administra las categorías de los productos</p>
+
+                                <Link
+                                    to="/admin/categories"
+                                    className="btn custom-btn"
+                                    onClick={scrollToTop}
+                                >
+                                    Ir a categorías
+                                    <i className="bi bi-tags ms-2" />
                                 </Link>
                             </div>
                         </div>
@@ -31,14 +51,34 @@ function AdminDashboard() {
                             <div className="card-body">
                                 <h2 className="card-title">Órdenes</h2>
 
-                                <p className="card-text">Administra las órdenes de los clientes.</p>
+                                <p className="card-text">Administra las órdenes de los clientes</p>
 
                                 <Link
                                     to="/admin/orders"
                                     className="btn custom-btn"
                                     onClick={scrollToTop}
                                 >
-                                    Administrar
+                                    Ir a órdenes
+                                    <i className="bi bi-cart4 ms-2" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div className="card custom-card mb-4">
+                            <div className="card-body">
+                                <h2 className="card-title">Usuarios</h2>
+
+                                <p className="card-text">Administra los usuarios de la tienda</p>
+
+                                <Link
+                                    to="/admin/users"
+                                    className="btn custom-btn"
+                                    onClick={scrollToTop}
+                                >
+                                    Ir a usuarios
+                                    <i className="bi bi-people ms-2" />
                                 </Link>
                             </div>
                         </div>
