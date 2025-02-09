9983f7bb04287c9584b04da94864ab52ae556547
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-09 20:21:49 -0300

    update styles

--- src/components/item-list/ItemList.jsx
+++ src/components/item-list/ItemList.jsx
@@ -4,11 +4,11 @@ function ItemList({ items }) {
     if (!items) return null;
 
     return (
-        <ul className="row justify-content-center g-5 m-3 mb-5 p-0">
+        <ul className="row justify-content-center g-3 m-5 p-0">
             {items.map((item) => (
                 <li
                     key={item.id}
-                    className="col-12 col-sm-6 col-md-4 col-lg-3"
+                    className="col-12 col-sm-6 col-md-4 col-lg-3 p-2"
                 >
                     <Item item={item} />
                 </li>
