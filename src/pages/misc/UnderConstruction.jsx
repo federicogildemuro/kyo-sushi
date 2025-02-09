7f8f2d1c1930e06d9205fb986e8538f89525f87f
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-09 20:23:48 -0300

    add under construction page

---
+++ src/pages/misc/UnderConstruction.jsx
@@ -0,0 +1,18 @@
+import BackButton from '../../components/BackButton';
+
+function NotFound() {
+    return (
+        <section className="d-flex flex-column text-center">
+            <div className="container">
+                <p className="fs-5 m-5">
+                    <i className="bi bi-exclamation-triangle-fill me-2" />
+                    Página en construcción
+                </p>
+
+                <BackButton />
+            </div>
+        </section>
+    );
+}
+
+export default NotFound;
