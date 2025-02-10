f074d2c6d26fc8e0fbd892aaf0bf58d44c90fb16
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-10 05:04:45 -0300

    refactor sort buttons component

--- src/components/sort-buttons/SortButtons.jsx
+++ src/components/sort-buttons/SortButtons.jsx
@@ -1,29 +1,29 @@
 import { useEffect } from 'react';
-import useSorting from '..//hooks/useSorting';
+import useSorting from '../../hooks/useSorting';
 
 function SortButtons({ items, onChange, fields }) {
     const { sortOrder, handleSort, applySorting } = useSorting();
 
     useEffect(() => {
         onChange(applySorting(items));
-    }, [items, onChange, applySorting]);
+    }, [onChange, applySorting, items]);
 
     const getArrowIcon = (field) => {
         if (sortOrder.field === field) {
             return sortOrder.direction === 'asc' ? (
-                <i className="bi bi-arrow-up"></i>
+                <i className="bi bi-arrow-up" />
             ) : (
-                <i className="bi bi-arrow-down"></i>
+                <i className="bi bi-arrow-down" />
             );
         }
-        return <i className="bi bi-arrow-down-up"></i>;
+        return <i className="bi bi-arrow-down-up" />;
     };
 
     return (
-        <div className="d-flex align-items-center justify-content-center gap-3">
+        <div className="d-flex flex-wrap gap-3 mb-5">
             {fields.map((field) => (
                 <button
-                    key={field.name}
+                    key={field.key}
                     className="btn custom-btn"
                     onClick={() => handleSort(field.key)}
                 >
