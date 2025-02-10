d068d3150662f3e8a825bc175da427a2956bf578
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-10 05:08:12 -0300

    update import

--- src/pages/store/StoreContent.jsx
+++ src/pages/store/StoreContent.jsx
@@ -1,5 +1,5 @@
 import ItemList from '../../components/item-list/ItemList';
-import Pagination from '../../components/Pagination';
+import Pagination from '../../components/pagination/Pagination';
 import ProductsNotFound from './ProductsNotFound';
 
 function StoreContent({ currentItems, totalPages, currentPage, setCurrentPage }) {
