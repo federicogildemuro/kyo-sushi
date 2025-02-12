b1ebe94daf5784f449fa11724c615803b3fa9499
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-12 09:26:50 -0300

    remove link

--- src/pages/store/ProductsNotFound.jsx
+++ src/pages/store/ProductsNotFound.jsx
@@ -1,6 +1,3 @@
-import { Link } from 'react-router-dom';
-import { scrollToTop } from '../../utils/scrollUtils';
-
 function ProductsNotFound() {
     return (
         <div className="text-center m-5">
@@ -8,15 +5,6 @@ function ProductsNotFound() {
                 <i className="bi bi-exclamation-triangle me-2" />
                 No se encontraron productos
             </p>
-
-            <Link
-                to="/tienda"
-                className="btn custom-btn"
-                onClick={scrollToTop}
-            >
-                Ir a la tienda
-                <i className="bi bi-shop ms-2" />
-            </Link>
         </div>
     );
 }
