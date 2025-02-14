d0be97a25405f369770ff7604ff834ccaeadfef0
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-14 11:34:13 -0300

    add animation

--- src/components/item-list/ItemList.jsx
+++ src/components/item-list/ItemList.jsx
@@ -1,20 +1,36 @@
+import { motion } from 'framer-motion';
 import Item from './Item';
 import './ItemList.css';
 
 function ItemList({ items }) {
     if (!items) return null;
 
+    const itemVariants = {
+        hidden: { opacity: 0, y: 25 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: { duration: .5, ease: 'easeInOut' }
+        },
+    };
+
     return (
-        <ul className="row justify-content-center g-3 mx-5 p-0">
+        <motion.ul
+            className="row justify-content-center g-3 mx-5 p-0"
+            initial="hidden"
+            animate="visible"
+            transition={{ staggerChildren: .25 }}
+        >
             {items.map((item) => (
-                <li
+                <motion.li
                     key={item.id}
                     className="col-12 col-sm-6 col-md-4 col-lg-3 p-2"
+                    variants={itemVariants}
                 >
                     <Item item={item} />
-                </li>
+                </motion.li>
             ))}
-        </ul>
+        </motion.ul>
     );
 }
 
