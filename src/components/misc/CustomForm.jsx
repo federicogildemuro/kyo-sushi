803c5cdb8cbacfa0316cc8e95d20ed9abe812d11
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-18 09:09:36 -0300

    add custom form component

---
+++ src/components/misc/CustomForm.jsx
@@ -0,0 +1,47 @@
+function CustomForm({ formConfig, formData, formErrors, handleInputChange, handleBlur, handleSubmit, submitText }) {
+    return (
+        <form
+            className="col-12 col-lg-6 mx-auto my-5"
+            onSubmit={handleSubmit}
+        >
+            {Object.entries(formConfig).map(([field, config]) => (
+                <div
+                    key={field}
+                    className="d-flex flex-column align-items-start mb-3"
+                >
+                    <label
+                        htmlFor={field}
+                        className="form-label"
+                    >
+                        {config.label}
+                    </label>
+
+                    <input
+                        type={config.type}
+                        id={field}
+                        className="form-control"
+                        name={field}
+                        value={formData[field]}
+                        onChange={handleInputChange}
+                        onBlur={handleBlur}
+                    />
+
+                    {formErrors[field] &&
+                        <div className="small text-danger text-start mt-1">
+                            {formErrors[field]}
+                        </div>
+                    }
+                </div>
+            ))}
+
+            <button
+                type="submit"
+                className="btn custom-btn"
+            >
+                {submitText}
+            </button>
+        </form>
+    );
+}
+
+export default CustomForm;
