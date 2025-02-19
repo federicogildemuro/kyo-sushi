9fd29a8e17c6d314d0ba2c36673e6fdef9c8dced
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-02-19 17:13:56 -0300

    update authenticated routes

--- src/routes/authenticatedRoutes.js
+++ src/routes/authenticatedRoutes.js
@@ -1,13 +1,17 @@
 import { lazy } from 'react';
 
-const UnderConstruction = lazy(() => import('../pages/misc/UnderConstruction'));
+const Profile = lazy(() => import('../pages/profile/Profile'));
+const EditProfile = lazy(() => import('../pages/profile/EditProfile'));
+const Orders = lazy(() => import('../pages/orders/Orders'));
 const Cart = lazy(() => import('../pages/cart/Cart'));
 const Favorites = lazy(() => import('../pages/favorites/Favorites'));
 const Checkout = lazy(() => import('../pages/checkout/Checkout'));
 const OrderConfirmation = lazy(() => import('../pages/checkout/OrderConfirmation'));
 
 const userRoutes = [
-    { path: '/profile', component: UnderConstruction },
+    { path: '/profile', component: Profile },
+    { path: '/profile/edit', component: EditProfile },
+    { path: '/profile/orders', component: Orders },
     { path: '/cart', component: Cart },
     { path: '/favorites', component: Favorites },
     { path: '/checkout', component: Checkout },
